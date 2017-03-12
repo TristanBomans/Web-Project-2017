0e6383fe79fd4e0c9f4c7ec10fc9a00df8907dea
DESKTOP-TRISTAN\Tristan <tristan.bomans@student.ehb.be>
2017-03-12 12:25:41 +0100

    css

--- Views/partials/review.php
+++ Views/partials/review.php
@@ -1,15 +1,15 @@
 <div id='detailwrap-detail'>
 	<form action="../Controllers/RequestController.php" method="POST">
 		<h2 id="h2-login">Schrijf een Review: </h2>
-		<div class="login-line-content">
+		<div class="login-line-content clearfix">
 			<p class="login-p">Rating: </p>
 			<input type="text" name="rating" class="login-input">
 		</div>
-		<div class="login-line-content">
+		<div class="login-line-content clearfix">
 			<p class="login-p">Opmerking: </p>
 			<input type="text" name="comment" class="login-input">
 		</div>
-		<div class="login-line-content-submit-button">
+		<div class="login-line-content-submit-button clearfix">
 			
 				<?php if(isset($_SESSION['user'])){
 					echo "<input type='submit'>";
