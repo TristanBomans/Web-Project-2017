3da9254cd270415bf2951add721794b7bd56207f
DESKTOP-TRISTAN\Tristan <tristan.bomans@student.ehb.be>
2017-04-13 00:39:37 +0200

    schoonheidsfixjes 2

--- Views/partials/comparableProds.php
+++ Views/partials/comparableProds.php
@@ -21,29 +21,28 @@ require_once "../Controllers/Util.php";
     {
         $productt->datum_toegevoegd =  explode("-",$productt->datum_toegevoegd)[2]."-".explode("-",$productt->datum_toegevoegd)[1]."-".explode("-",$productt->datum_toegevoegd)[0];
         
-        $html .= "<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12 parent-thumb-cont clearfix'>"
+        $html .= "<a href='/Views/detail?opgevraagdProduct=".$productt->id."'><div class='col-lg-3 col-md-4 col-sm-6 col-xs-12 parent-thumb-cont clearfix'>"
          . "<div class='thumbnail thumb-cont'>"
-         . "<div class='thumb-cont-money-circle'>€ ".$productt->prijs."</div>"
+         . "<div class='thumb-cont-money-circle'>€ ".round($productt->prijs, 1)."</div>"
          . "<img src='".$productt->img_path."'class='image-thumb' alt='Deze afbeelding kon niet gevonden worden'>"
          . "<div class='caption'>"
          . "<h3>".$productt->naam."</h3>"
          . "<div class='wrapper-date-cat'><b>".$productt->datum_toegevoegd."</b>"
          . "<b class='thumb-categorie'>".$productt->cat_naam."</b></div>"
-         . "<a value='Detail' class='btn btn-default btn-lg btn-detail' href='detail.php?opgevraagdProduct=".$productt->id."'>Detail</a>"
          . "</div>"   
          . "</div>"
          . "<form action='../Controllers/RequestController.php' method='POST'>"
          . "<input type='hidden' name='toAddProduct' value='".$productt->id."'>"
          . "<input type='submit' value='' class='winkelwagen-btn winkelwagen-add' title='Voeg toe aan winkelmandje'>";
           if ($productt->avg_rating != 0) {
-            $html .= "<div class='product-rating-icon'>".$productt->avg_rating."</div>";
+            $html .= "<div class='product-rating-icon'>".round($productt->avg_rating, 1)."</div>";
           }
          $html.= "</form>"
          ."</form>"
          . "<div class='product-metadata-id metadata'>".$productt->id."</div>"
          . "<div class='product-metadata-naam metadata'>".$productt->naam."</div>"
          . "<div class='product-metadata-prijs metadata'>".$productt->prijs."</div>"
-         . "</div>";
+         . "</div></a>";
     }
     echo $html;
     ?>
